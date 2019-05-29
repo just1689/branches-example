eec0d7a02edd9c2f4a64bb2c980790b13866bb3a
Justin Tamblyn <just1689@gmail.com>
2019-05-29 14:58:16 +0200

    Added comment

--- account.sql
+++ account.sql
@@ -2,6 +2,7 @@ create or replace package account as
 begin
     create function GetAccountBalance(ID VARCHAR2(20 CHAR)) as
         begin
+            --COMMENT
             select balance into RET from account where id = ID order by created_date desc;
             RETURN RET
         end
